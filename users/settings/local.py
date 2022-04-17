5dd25ceb792822cb173d46911d196f2699124d51
codigoenlaweb <codeenlaweb@gmail.com>
2022-04-16 21:09:29 -0400

    settings

---
+++ users/settings/local.py
@@ -0,0 +1,30 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'users-dj',
+        'USER': 'postgres',
+        'PASSWORD': 'olmosolmos',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR.parent / "static"]
+
+# media bs-data
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.parent / "media"
